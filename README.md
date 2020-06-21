96e932dfffe885e14e2d3cde7395faca4822862d
kangzhiq <709563092@qq.com>
2020-06-21 20:34:40 +0200

    Preprocessing of the dataset

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ This is my GSoC2020 project with Red Hen Lab.
 
 The goal is to design a network capable of detecting and recognizing hand gestures and then apply it to annotate the dataset of news videos of Red Hen Lab.
 
+# Preprocessing of Dataset IEMOCAP
+
+```mocap_data_collect.py``` extract information from the IEMOCAP dataset and save the data as pickle.
+
+# Audio-Visual emtion recognition
+
+Audio-only, visual-only and audio-visual models are tested to verify the utility of bimodal information.
---
+++ video2landmarks.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 20:15:10 2020
+
+@author: zhiqi
+"""
+
+import numpy as np
+import cv2
+import face_alignment
+import pickle
+
+import argparse
+import tqdm
+
+
+def load_args():
+    parser = argparse.ArgumentParser(description="Video2landmarks")
+    parser.add_argument('--video_path', type=str, default='', help="Path of the video")
+    parser.add_argument('--device', type=str, default='cpu', help="'cpu' or 'cuda'")
+    parser.add_argument('--save_path', type=str, default=None, help="Path to save transformed frames")
+
+    args = parser.parse_args()
+    
+    return args
+
+def save_frames(video_path, frame_path):
+    vidcap = cv2.VideoCapture(video_path)
+    count = 0
+    success,image = vidcap.read()
+    while success:
+      cv2.imwrite(frame_path + "frame%03d.jpg" % count, image) # save frame as JPEG file
+      success,image = vidcap.read()           
+      count += 1
+
+
+def video2landmarks(video_path, save_path, device):           
+    LMs = []    
+    init_frame = []
+
+    vidcap = cv2.VideoCapture(video_path)
+    count = 0
+    success,image = vidcap.read()
+    u_max, v_max = image.shape[:2]
+    while success:
+        init_frame.append(image)
+        success,image = vidcap.read()
+        count += 1
+    
+    # Extracting landmarks
+    print("Extracting landmarks:")
+    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='gpu')
+    for i in tqdm(range(len(init_frame)), position=0, leave=True):
+        LMs.append(fa.get_landmarks(init_frame[i])[0])
+        
+    if save_path:
+        with open(save_path, 'wb') as handle:
+            pickle.dump(LMs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            
+    else:
+        print("No save path!")
+    
+    
+
+def main():
+    args = load_args()
+    print(args)
+    video2landmarks(args.video_path,
+                    save_path = args.save_path, 
+                    device = args.device)
+    
+
+if __name__ == "__main__":
+    main()
